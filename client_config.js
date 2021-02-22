ae98355b1f0b95019c20d259babf991641e14992
jmayor <john@johnmayor.ca>
2021-02-22 15:49:23 -0500

    Update client_config.js

--- client_config.js
+++ client_config.js
@@ -1,7 +1,7 @@
 function config() {
 	
 	return {
-		max_running: 50,
+		max_running: 51,
 		invalid_body_percent: 1,
 		feed_url: 'http://144.202.27.46/openrtb/?response_time=300&timeout_percent=2&coverage_percent=30&response_header_error_percent=5&response_json_error_percent=1&no_response_percent=10',
 		base_imp_url: 'https://impression.v6test.datablocks.net/i/',
